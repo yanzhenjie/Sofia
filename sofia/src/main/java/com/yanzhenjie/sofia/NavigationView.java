ae89f274ed2961f91bdef413a88cfa77f1e0e47a
YanZhenjie <smallajax@foxmail.com>
2018-04-03 19:19:01 +0800

    Fixed #18, fixed #13, fixed #10.

--- sofia/src/main/java/com/yanzhenjie/sofia/NavigationView.java
+++ sofia/src/main/java/com/yanzhenjie/sofia/NavigationView.java
@@ -15,15 +15,30 @@
  */
 package com.yanzhenjie.sofia;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.ContentObserver;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
+import android.provider.Settings;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by YanZhenjie on 2017/8/1.
  */
-public class NavigationView extends MeasureView {
+public class NavigationView extends View {
+
+    private static final String MINI_CONSTANT = "navigationbar_is_min";
+
+    private ContentResolver mResolver;
+    private WindowManager mWindowManager;
+    private int mNavigationBarSize;
 
     public NavigationView(Context context) {
         this(context, null, 0);
@@ -35,15 +50,59 @@ public class NavigationView extends MeasureView {
 
     public NavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mResolver = getContext().getContentResolver();
+        mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+
+        registerListener();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), sNavigationBarHeight);
+            measureNavigation();
+            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), mNavigationBarSize);
         } else {
             setMeasuredDimension(0, 0);
-            setVisibility(View.GONE);
         }
+    }
+
+    /**
+     * Get navigation bar height.
+     */
+    public int getNavigationBarSize() {
+        return mNavigationBarSize;
+    }
+
+    /**
+     * Listen to the navigation bar.
+     */
+    private void registerListener() {
+        Uri uri = Settings.System.getUriFor(MINI_CONSTANT);
+        mResolver.registerContentObserver(uri, true, new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                requestLayout();
+            }
+        });
+    }
+
+    /**
+     * Measure the size of the navigation bar.
+     */
+    private void measureNavigation() {
+        Display display = mWindowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealMetrics(displayMetrics);
+        } else {
+            display.getMetrics(displayMetrics);
+        }
+        mNavigationBarSize = displayMetrics.heightPixels - getDisplayHeight(display);
+    }
+
+    private static int getDisplayHeight(Display display) {
+        Point point = new Point();
+        display.getSize(point);
+        return point.y;
     }
 }
--- sofia/src/main/java/com/yanzhenjie/sofia/StatusView.java
+++ sofia/src/main/java/com/yanzhenjie/sofia/StatusView.java
@@ -16,6 +16,7 @@
 package com.yanzhenjie.sofia;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,7 +24,9 @@ import android.view.View;
 /**
  * Created by YanZhenjie on 2017/8/1.
  */
-public class StatusView extends MeasureView {
+public class StatusView extends View {
+
+    private int mStatusBarSize;
 
     public StatusView(Context context) {
         this(context, null, 0);
@@ -35,15 +38,25 @@ public class StatusView extends MeasureView {
 
     public StatusView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        Resources resources = getResources();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        mStatusBarSize = resources.getDimensionPixelSize(resourceId);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), sStatusBarHeight);
+            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), mStatusBarSize);
         } else {
             setMeasuredDimension(0, 0);
-            setVisibility(View.GONE);
         }
+    }
+
+    /**
+     * Get status bar height.
+     */
+    public int getStatusBarSize() {
+        return mStatusBarSize;
     }
 }
